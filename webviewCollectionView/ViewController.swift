83fa1c29af87c5d40e9c428c18bb7f7437d128dc
Jacob Bailosky <jakeb@jacobs-macbook-pro.local>
2018-12-11 11:54:44 -0500

    Initial Commit

---
+++ webviewCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  webviewCollectionView
+//
+//  Created by Jacob Bailosky on 12/11/18.
+//  Copyright © 2018 Jacob Bailosky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
